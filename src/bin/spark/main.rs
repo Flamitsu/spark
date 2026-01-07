383002cf4354581f34c895d7845f5c40c8de33f9
Flamitsu <flamitsu@proton.me>
2026-01-07 14:21:17 +0100

    Spark now have arguments (they only print in the console for now). This is going to be needed for installation process, detect kernels when they are installed, and remove the current Spark installation if needed.

--- src/bin/spark/main.rs
+++ src/bin/spark/main.rs
@@ -1,4 +1,33 @@
 // File where is going to reside the code of the general binary
-fn main(){
-   println!("")
+use std::env;
+use std::process::exit;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        run_default();
+        return;
+    }
+    // Arguments
+    match args[1].as_str() {
+        "install" => install(),
+        "remove"  => remove(),
+        _ => {
+            eprintln!("Unknown argument: {}", args[1]);
+            exit(1);
+        }
+    }
+}
+// Default argument (non argument)
+fn run_default() {
+    println!("Detect kernels...");
+}
+// Install argument
+fn install() {
+    println!("Installing the EFI binary...");
+}
+// Remove argument
+fn remove() {
+    println!("Remove");
 }
