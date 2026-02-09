2c33408bea41e771e8280f2ec36c20eaa035fda7
Flamitsu <flamitsu@proton.me>
2026-02-09 17:33:00 +0100

    Renamed execution.sh to install.sh . Improved documentation in os.rs

--- src/bin/spark/os.rs
+++ src/bin/spark/os.rs
@@ -5,14 +5,16 @@ pub enum GetDistroInfo{
     _Name
 }
 
-/// Gets the id of the distribution that is running the program
+/// Gets the attribute needed of the distribution that is running the program
 pub fn get_distro_information(attribute: GetDistroInfo) -> String {
+    // Opens the file 
     let os_config_file = if let Ok(file) = fs::read_to_string(get_route_os_release_file()){
         file
     } else{
         eprintln!("Error, unable to determine the distribution on which the programe is running. Proceeding with 'unknown'.");
         return "unknown".to_string();
     };
+    // Match the keys to know which attribute is needed  
     let match_key = match attribute{
         GetDistroInfo::_Name => {"NAME="},
         GetDistroInfo::Id => {"ID="}
@@ -21,6 +23,7 @@ pub fn get_distro_information(attribute: GetDistroInfo) -> String {
         if line.starts_with("#"){
             continue;
         }
+        // Returns the slice of the string 
         if line.starts_with(match_key){
             return line[match_key.len()..].to_string();
         }
@@ -29,7 +32,8 @@ pub fn get_distro_information(attribute: GetDistroInfo) -> String {
     eprintln!("Error, unable to determine the distribution on which the program is running. Proceeding with 'unknown'.");
     return "unknown".to_string();
 }
-/// This function tries to read the content from os-release file. 
+
+/// This function returns the possible route of the os-release file. 
 fn get_route_os_release_file() -> String {
     // Declares the route of the os-release file
     let mut path = "/etc/os-release";
@@ -37,6 +41,7 @@ fn get_route_os_release_file() -> String {
     if exists(Some(&path)) {
         return String::from(path);
     }
+    // If it does not find anything, tries to send this route.
     path = "/usr/lib/os-release";
     return String::from(path);
 }
