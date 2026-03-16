0762628d00c972b7a2ba0ce08ae56118f6ad68ee
Flamitsu <flamitsu@proton.me>
2026-03-16 20:42:04 +0100

    feat: detects if the disk is a valid GPT device and not MBR

--- spark-cli/src/boot/gpt.rs
+++ spark-cli/src/boot/gpt.rs
@@ -1,20 +1,29 @@
 // This archive is used to know which device is the correct for the installation of the ESP
 use crate::errors::SparkError;
-use std::fs::read_dir;
-
+use std::fs;
+use std::fs::File;
+use std::io::{Seek,SeekFrom,Read};
 /// Returns the actual ESP device, so the esp.rs module can discover its path.
 pub fn compatible_esp_device() -> Result<String, SparkError>{
     const BLOCK_DEV_ROUTE: &str = "/sys/block/";
     const LOGICAL_BLOCK: &str = "/queue/logical_block_size";
     let aviable_disks = get_disks(BLOCK_DEV_ROUTE)?;
-    
+
+    // Iterates the aviable disks in the system (plugged in)
     for disk in aviable_disks{
-        let dev_disk: String = format!("/dev/{}",disk);
-        let block_size: u16 = get_disk_logical_sector_size(&disk,BLOCK_DEV_ROUTE,LOGICAL_BLOCK)?;
-        let _number_partitions: u8 = get_number_partitions(block_size,&dev_disk)?;
+        let dev_route_disk: String = format!("/dev/{}",disk);
+        let block_size: u64 = get_disk_logical_sector_size(&disk,BLOCK_DEV_ROUTE,LOGICAL_BLOCK)?;
+        let open_disk = File::open(&dev_route_disk)?;
+        // If the disk is not a GPT valid disk, it will jump to the next one 
+        if !try_gpt_sectors(block_size, open_disk)?{
+            eprintln!("Warning: the disk {} may not be a valid GPT. Skipping...",dev_route_disk);
+            continue
+        }
+
+        let _number_partitions: u8 = get_number_partitions(block_size,&dev_route_disk)?;
         println!("{} LBA Size: {}",disk,block_size);
     }
-
+    
     Ok(String::new())
 }
 
@@ -23,7 +32,7 @@ fn get_disks(block_route: &str) -> Result<Vec<String>, SparkError>{
     // Creates a new empty vec where the found disk devices are going to be storaged
     let mut disks:Vec<String> = Vec::new();
     // Creates an empty vec
-    let disk_devices = read_dir(block_route)?; 
+    let disk_devices = fs::read_dir(block_route)?; 
     for device in disk_devices{
         
         // Unpacks the device, if there is an error it will jump to the next one 
@@ -62,23 +71,38 @@ fn check_valid_block_name(device_name: &str) -> bool {
 
 /// Get disk logical sector size (Example: 512, 4096...)
 fn get_disk_logical_sector_size(disk_block: &str, block_route: &str, 
-    logical_block_route: &str) -> Result<u16, SparkError>{
+    logical_block_route: &str) -> Result<u64, SparkError>{
+    
     // The route where the logical block sector size is storaged in
     let complete_route = format!("{}{}{}",block_route,disk_block,logical_block_route);
     // Gets the LBA sector size value and converts it to a string
-    let value : u16 = std::fs::read_to_string(complete_route)?.trim().parse()?;
+    let value : u64 = std::fs::read_to_string(complete_route)?.trim().parse()?;
+    
     Ok(value)
 }
 
-#[allow(unused)]
-/// Gets the proper bytes from the disk to know if it is a good GPT and have LBA0 MBR protective 
-fn try_gpt_sectors(logical_block_size: u16) -> bool{
-    false
+/// Gets the proper bytes from the disk to know if it is a good GPT and have LBA1 sign "EFI PART" 
+fn try_gpt_sectors(logical_block_size: u64, mut open_disk: File) -> Result<bool, SparkError>{
+    
+    // The EFI part signature. "EFI PART" in ascii, 
+    const EFI_PART_SIGN: [u8;8] = [0x45, 0x46, 0x49, 0x20, 0x50, 0x41, 0x52, 0x54];
+    let mut buffer = [0u8;8];
+    
+    // Moves the pointer to the logical_block_size value (byte 512... 4096...)
+    open_disk.seek(SeekFrom::Start(logical_block_size))?;
+    // Reads the exact buffer and storages it 
+    open_disk.read_exact(&mut buffer)?;
+    
+    // If the buffer matches with the EFI_PART_SIGN then it is a valid GPT partition.
+    if buffer == EFI_PART_SIGN{
+        return Ok(true)
+    }
+    Ok(false)
 }
 
 #[allow(unused)]
 /// This function returns the number of partitions of an specific device.
-fn get_number_partitions(logical_block_size: u16, disk: &str) -> Result<u8, SparkError>{
+fn get_number_partitions(logical_block_size: u64, disk: &str) -> Result<u8, SparkError>{
     Ok(1)
 }
 
