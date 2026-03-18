01662f8a947265116811e55147734622ca50c035
Flamitsu <flamitsu@proton.me>
2026-03-18 22:23:14 +0100

    feat: reads the raw bytes to match the ESP sign in the disk.

--- spark-cli/src/boot/gpt.rs
+++ spark-cli/src/boot/gpt.rs
@@ -1,8 +1,9 @@
 // This archive is used to know which device is the correct for the installation of the ESP
 use crate::errors::SparkError;
-use std::fs;
+use std::fs::read_dir;
 use std::fs::File;
 use std::io::{Seek,SeekFrom,Read};
+
 /// Returns the actual ESP device, so the esp.rs module can discover its path.
 pub fn compatible_esp_device() -> Result<String, SparkError>{
     const BLOCK_DEV_ROUTE: &str = "/sys/block/";
@@ -11,17 +12,18 @@ pub fn compatible_esp_device() -> Result<String, SparkError>{
 
     // Iterates the aviable disks in the system (plugged in)
     for disk in aviable_disks{
+        
         let dev_route_disk: String = format!("/dev/{}",disk);
+        // You may wonder why this is u64, whenever you use the seek method you need a u64 int
         let block_size: u64 = get_disk_logical_sector_size(&disk,BLOCK_DEV_ROUTE,LOGICAL_BLOCK)?;
         let open_disk = File::open(&dev_route_disk)?;
+        
         // If the disk is not a GPT valid disk, it will jump to the next one 
-        if !try_gpt_sectors(block_size, open_disk)?{
+        if !try_gpt_sectors(block_size, &open_disk)?{
             eprintln!("Warning: the disk {} may not be a valid GPT. Skipping...",dev_route_disk);
             continue
         }
 
-        let _number_partitions: u8 = get_number_partitions(block_size,&dev_route_disk)?;
-        println!("{} LBA Size: {}",disk,block_size);
     }
     
     Ok(String::new())
@@ -31,8 +33,8 @@ pub fn compatible_esp_device() -> Result<String, SparkError>{
 fn get_disks(block_route: &str) -> Result<Vec<String>, SparkError>{
     // Creates a new empty vec where the found disk devices are going to be storaged
     let mut disks:Vec<String> = Vec::new();
-    // Creates an empty vec
-    let disk_devices = fs::read_dir(block_route)?; 
+    // Creates an empty vec to storage the disks
+    let disk_devices = read_dir(block_route)?; 
     for device in disk_devices{
         
         // Unpacks the device, if there is an error it will jump to the next one 
@@ -82,9 +84,9 @@ fn get_disk_logical_sector_size(disk_block: &str, block_route: &str,
 }
 
 /// Gets the proper bytes from the disk to know if it is a good GPT and have LBA1 sign "EFI PART" 
-fn try_gpt_sectors(logical_block_size: u64, mut open_disk: File) -> Result<bool, SparkError>{
+fn try_gpt_sectors(logical_block_size: u64, mut open_disk: &File) -> Result<bool, SparkError>{
     
-    // The EFI part signature. "EFI PART" in ascii, 
+    // The EFI part signature. "EFI PART" in ascii, (This is in raw bytes) 
     const EFI_PART_SIGN: [u8;8] = [0x45, 0x46, 0x49, 0x20, 0x50, 0x41, 0x52, 0x54];
     let mut buffer = [0u8;8];
     
@@ -101,13 +103,43 @@ fn try_gpt_sectors(logical_block_size: u64, mut open_disk: File) -> Result<bool,
 }
 
 #[allow(unused)]
-/// This function returns the number of partitions of an specific device.
-fn get_number_partitions(logical_block_size: u64, disk: &str) -> Result<u8, SparkError>{
-    Ok(1)
-}
+/// Checks and compare the partition type GUID and returns the partition number 
+fn find_uefi_guid(logical_block_size: u64, mut open_disk:&File) -> Result<Option<u8>, SparkError>{ 
+    // This number represent where the partitions info begin. (LBA2)
+    const LBA: u64 = 2;
+    // Maximum partition for device in GPT
+    const MAX_PARTITIONS: u8 = 128;
+    /*
+     * This is the partition sign for the UEFI partition
+     * All this values from the last GUID in LE to the first:
+     * LE and BE (GUID RAW): 28 73 2A C1 1F F8 11 D2 BA 4B 00 A0 C9 3E C9 3B 
+     * String (GUID LE and BE to String): C12A7328 1FF8 D211 BA4B 00A0C93EC93B 
+    */
+    const ESP_GUID_BYTES: [u8;16]= [
+        0x28, 0x73, 0x2A, 0xC1, // (LE) DATASET1 -> 28 73 2A C1 -> C12A7328 
+        0x1F, 0xF8, // (LE) DATASET2 -> 1F F8 -> 1FF8
+        0xD2, 0x11, // (LE) DATASET3 -> 11 D2 -> D211
+        0xBA, 0x4B, // (BE) DATASET4 -> BA 4B -> BA4B
+        0x00, 0xA0, 0xC9, 0x3E, 0xC9, 0x3B // (BE) DATASET5 -> 00 A0 C9 3E C9 3B -> 00A0C93EC93B  
+    ];
+    let offset: u64 = logical_block_size * LBA;
+    // This counter will go up every partition sector that isn't empty.
+    let mut partitions = 0;
+    for _ in 0..MAX_PARTITIONS {
+        let mut buffer = [0u8;16];
+        open_disk.seek(SeekFrom::Start(offset))?;
+        open_disk.read_exact(&mut buffer)?;
+        // If the partition sign isn't empty then its a partition. 
+        if buffer != [0u8;16]{
+            partitions = partitions + 1;
+        }
+        // If the buffer matches the sign, then it is an ESP valid partition.
+        if buffer == ESP_GUID_BYTES{
+            return Ok(Some(partitions));
+        }
+    // Moves the cursor 112 bytes in the disk. If there is any error it will skip the iteration 
+        open_disk.seek(SeekFrom::Current(112))?;
+    }
 
-#[allow(unused)]
-/// Checks and compare the partition type GUID
-fn check_uefi_guid() -> Result<(), SparkError>{
-    Ok(())
+    Ok(None)
 }
