48fcc4df501c58ec092e50a7f1f65a634c892c94
Flamitsu <flamitsu@proton.me>
2026-04-02 22:42:48 +0200

    feat: CRC32 array checksum should work. Add new config variables to the config.rs file. Working on new implementations in disk.rs to finally complete the gpt.rs module.

--- ignix-cli/src/boot/gpt.rs
+++ ignix-cli/src/boot/gpt.rs
@@ -1,20 +1,19 @@
 use crate::boot::disk;
-use crate::config::{BLOCK_DEV_ROUTE, LOGICAL_BLOCK, EFI_PART_SIGN, MAX_BUFFER_SIZE, MAX_HEADER_SIZE};
+use crate::config::{BLOCK_DEV_ROUTE, LOGICAL_BLOCK, EFI_PART_SIGN, MAX_BUFFER_SIZE, MAX_ARRAY_PART_SIZE, MAX_GPT_HEADER_SIZE, ESP_GUID_BYTES, DEVNAME, DEVTYPE, PARTUUID};
 use crate::errors::{IgnixError, io};
 use crate::errors::cmd;
 use crate::boot::crc32::calculate_crc32;
 use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
+use std::path::Path;
 
 pub fn compatible_esp_partition(devices: Vec<String>) -> Result<String, IgnixError>{
-
     for device in devices {
-        
         let sector_size = get_disk_sector_size(&device, BLOCK_DEV_ROUTE, LOGICAL_BLOCK)?;
-        let disk = File::open(format!("/dev/{}",device))?; 
+        let mut disk = File::open(format!("/dev/{}",device))?;
         let buffer = get_gpt_structure(sector_size, &disk)?;
         
-        if !is_efi_signed(buffer)?{
+        if !is_disk_efi_signed(buffer)?{
             continue;
         }
         
@@ -26,16 +25,21 @@ pub fn compatible_esp_partition(devices: Vec<String>) -> Result<String, IgnixErr
 
         // The standard says here the max should be 128, however it is dynamic here just in case.
         let gpt_max_partitions: u32 = get_max_gpt_partition(buffer)?;
-        let gpt_max_partition_size: u32 = get_partition_max_size(buffer)?;
+        let gpt_entry_size: u32 = get_partition_max_size(buffer)?;
         let part_array_start: u64 = get_partition_array_start(buffer)?;
+        
+        if !validate_crc32_partition_array_checksum(buffer, gpt_max_partitions, gpt_entry_size, part_array_start, sector_size)?{
+            continue;
+        }
 
+        let Some(guid) = get_esp_guid(&buffer, gpt_max_partitions, gpt_entry_size, sector_size, part_array_start)? else { continue; };
+        let partition_guid_string = format_partuuid(&guid)?;
     }
-    
     Err(cmd::Error::NotEFIPartitionFound)?
 }
 
 
-fn is_efi_signed(buffer: [u8;MAX_BUFFER_SIZE]) -> Result<bool, IgnixError>{
+fn is_disk_efi_signed(buffer: [u8;MAX_BUFFER_SIZE]) -> Result<bool, IgnixError>{
     if buffer[0..8] != EFI_PART_SIGN{
         return Ok(false);
     }
@@ -49,7 +53,7 @@ fn validate_crc32_header_checksum(buffer:[u8;MAX_BUFFER_SIZE], header_size: u32)
     let stored_crc = u32::from_le_bytes(buffer[16..20].try_into()?);
     /* Copies only the header to change the header checksum field to 0, that is how the CRC32
      * checksums says is correct.*/
-    let mut header_copy = [0u8; MAX_HEADER_SIZE];
+    let mut header_copy = [0u8; MAX_GPT_HEADER_SIZE];
     header_copy[..size].copy_from_slice(&buffer[..size]);
     header_copy[16..20].fill(0);
     
@@ -62,21 +66,74 @@ fn validate_crc32_header_checksum(buffer:[u8;MAX_BUFFER_SIZE], header_size: u32)
     Ok(false)
 }
 
-fn validate_crc32_partition_array_checksum(buffer: [u8;MAX_BUFFER_SIZE], header_size: u32) 
--> Result<bool, IgnixError> {
+fn validate_crc32_partition_array_checksum(buffer: [u8;MAX_BUFFER_SIZE], gpt_max_partitions: u32, gpt_entry_size: u32, part_array_start: u64, sector_size: u64) -> Result<bool, IgnixError>{ 
+    // Checks if it overflows and it is freed instantly after.
+    {
+        let bytes_of_array = gpt_max_partitions as u64 * gpt_entry_size as u64 + sector_size;
+        if bytes_of_array as usize > MAX_BUFFER_SIZE {
+            Err(io::Error::InvalidBuffer("Partition array buffer overflows.".to_string()))?;
+        }
+    }
+
+    let offset = sector_size as usize;
     let part_array_crc = u32::from_le_bytes(buffer[88..92].try_into()?);
-    calculate_crc32(&buffer[header_size as usize..MAX_BUFFER_SIZE]);
-    Ok(true)
+    // calculates it as long as the header isn't 
+    let crc32 = calculate_crc32(&buffer[offset..MAX_BUFFER_SIZE]);
+    
+    if part_array_crc == crc32 {
+        return Ok(true);
+    }
+    Ok(false)
 }
 
+fn get_esp_guid(buffer: &[u8;MAX_BUFFER_SIZE], gpt_max_partitions: u32, gpt_entry_size: u32, sector_size: u64, part_array_start: u64) -> Result<Option<[u8;16]>, IgnixError>{
+    // This -1 here is because the iteration already skips the first LBA0 (MBR PROTECTIVE) sector.
+    let offset = sector_size * part_array_start - 1;
+    
+    for partition in 0..gpt_max_partitions{
+        let entry_start = offset as usize + (partition as usize * gpt_entry_size as usize);
+        let entry_end = entry_start + 16;
+        
+        if entry_start > MAX_BUFFER_SIZE{
+            Err(io::Error::InvalidBuffer("Invalid buffer while parsing the partition entries. (too long.)".to_string()))?
+        }
+        
+        let gpt_array_header = &buffer[entry_start..entry_end];
+        let type_guid = &gpt_array_header[0..16];
+        
+        if type_guid == ESP_GUID_BYTES{
+            let unique_guid: [u8;16] = gpt_array_header[16..32].try_into()?;
+            return Ok(Some(unique_guid))
+        }
+
+    }
+    Ok(None)
+}
+
+fn get_esp_partition(device: &str, guid: &str) -> Result<String, IgnixError>{
+    Ok(format!(""))
+}
+
+fn format_partuuid(guid: &[u8;16]) -> Result<String, IgnixError>{
+    let data1 = u32::from_le_bytes(guid[0..4].try_into()?);
+    let data2 = u16::from_le_bytes(guid[4..6].try_into()?);
+    let data3 = u16::from_le_bytes(guid[6..8].try_into()?);
+    Ok(
+        // If the field isn't big enough the format says to add padding. x is for small letters.
+        format!(
+        "{:08x}-{:04x}-{:04x}-{:02x}{:02x}-{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
+        data1, data2, data3,
+        guid[8], guid[9],
+        guid[10], guid[11], guid[12], guid[13], guid[14], guid[15]
+        )
+    )
+}
 
 fn get_gpt_header_size(buffer: [u8;MAX_BUFFER_SIZE]) -> Result<u32, IgnixError>{
     let header_size = u32::from_le_bytes(buffer[12..16].try_into()?);
- 
-    if header_size as usize >= MAX_HEADER_SIZE {
+    if header_size as usize > MAX_GPT_HEADER_SIZE {
         return Err(io::Error::InvalidBuffer("Header size overflows.".to_string()))?
     }
-
     Ok(header_size)
 }
 
@@ -101,10 +158,15 @@ fn get_gpt_structure(lba_size: u64, mut disk: &File) -> Result<[u8;MAX_BUFFER_SI
     Ok(buffer)
 }
 
+/*
+ * If you're wondering why this is a string and not a 'Path' as it should be,
+ * it's because Rust is being bitchy about that it doesn't know a 'Path' size in
+ * compilation time.
+ * */
 fn get_disk_sector_size(disk: &str, block_route: &str, lba_size_route: &str)
     -> Result<u64, IgnixError>{
     // Example: /sys/class/block/nvme0n1/queue/logical_block_size
-    let sector_size_path = &format!(r"{}{}{}",block_route,disk,lba_size_route);
+    let sector_size_path = format!(r"{}{}{}",block_route,disk,lba_size_route);
     // Reads the value into a string and parses it into an unsigned int value.
     let value: u64 = std::fs::read_to_string(sector_size_path)?.trim().parse()?;
     
--- ignix-cli/src/config.rs
+++ ignix-cli/src/config.rs
@@ -1,41 +1,49 @@
 // This is the archive where all the config of the program resides. 
+pub const DEVNAME: &str = "DEVNAME=";
+pub const DEVTYPE: &str = "DEVTYPE=";
+pub const PARTUUID: &str = "PARTUUID=";
 
 // This constants are being used in gpt.rs at this moment
 /// EFI part signature in array of integer bytes.
 pub const EFI_PART_SIGN: [u8;8] = *b"EFI PART";
-/// GPT max header size. Is duplicated from the std one. (92*2)
-pub const MAX_HEADER_SIZE: usize = 184;
-/// GPT max partition array size. Is 128 bytes per entry and 128 bytes the std of GPT.
-const MAX_PARTITION_ARRAY_SIZE: usize = 16384;
 
-pub const MAX_BUFFER_SIZE: usize = MAX_HEADER_SIZE + MAX_PARTITION_ARRAY_SIZE;
+pub const ESP_GUID_BYTES: [u8;16]= [
+        0x28, 0x73, 0x2A, 0xC1, // (LE) DATASET1 -> 28 73 2A C1 -> C12A7328 
+        0x1F, 0xF8, // (LE) DATASET2 -> 1F F8 -> 1FF8
+        0xD2, 0x11, // (LE) DATASET3 -> 11 D2 -> D211
+        0xBA, 0x4B, // (BE) DATASET4 -> BA 4B -> BA4B
+        0x00, 0xA0, 0xC9, 0x3E, 0xC9, 0x3B // (BE) DATASET5 -> 00 A0 C9 3E C9 3B -> 00A0C93EC93B  
+    ];
+// Measures are in bytes.
+const MAX_GPT_PARTITIONS: usize = 128;
+const MAX_GPT_PARTITION_ENTRY_SIZE: usize = 128;
+const MAX_LBA_SECTOR_SIZE: usize = 4096;
 
+pub const MAX_GPT_HEADER_SIZE: usize = 92;
+pub const MAX_ARRAY_PART_SIZE: usize = MAX_GPT_PARTITIONS * MAX_GPT_PARTITION_ENTRY_SIZE;
+// This buffer size is here for security. If this was a heap, will potentially make the code run slower and can expose a thread to systems with corrupt disks.
+pub const MAX_BUFFER_SIZE: usize = MAX_GPT_PARTITIONS * MAX_GPT_PARTITION_ENTRY_SIZE + MAX_LBA_SECTOR_SIZE;
+
+// Routes 
 /// Default efi bin path that defines where the EFI binary should be located (default and fallback)
 pub const DEFAULT_EFI_BIN_PATH: &str = "/usr/lib/ignix/ignixx64.efi";
-
-/// This flag is what the user should put as an argument to give a distinct EFI bin path.
-pub const EFI_BIN_PATH: &str = "--efi-bin=";
-
 /// Defines the route that looks up the program to search for real block devices.
 pub const BLOCK_DEV_ROUTE: &str = "/sys/class/block/";
-
 /// Defines the route '/sys/class/block/{device}/queue/logical_block_size' with format to get the sector block size
 pub const LOGICAL_BLOCK: &str = "/queue/logical_block_size";
 
+// Arguments 
 /// Neccesary for the confirmation flags in the CLI usage of the tool while the install process. 
 pub const SHORT_CONFIRMATION_FLAG: &str = "-y";
-
 /// Neccesary for the confirmation flags in the CLI usage of the tool while the install process.
 pub const LONG_CONFIRMATION_FLAG: &str = "--yes";
-
 /// Flag to allow the program to install in a virtual device.
 pub const ALLOW_VIRTUAL_FLAG: &str = "--allow-virtual";
-
 /// Flag argument to remove the neccesary NVRAM write variable.
 pub const NO_NVRAM: &str = "--no-nvram";
-
 /// Flag argument to say which route should the program be installed in.
 pub const INSTALL_ROUTE: &str = "--install-route=";
-
+/// This flag is what the user should put as an argument to give a distinct EFI bin path (install).
+pub const EFI_BIN_PATH: &str = "--efi-bin=";
 /// Flag argument to install the bootloader in a removable device.
 pub const REMOVABLE_FLAG: &str = "--removable";
